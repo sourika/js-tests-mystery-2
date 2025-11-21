9e4942a66fb91d813a3e32ea102ef59af9156406
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-11-21 09:19:51 -0800

    Implement leap year determination logic in mystery2 function

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,9 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  if (num % 400 === 0) {
+    return true;
+  } else if (num % 100 === 0 || num % 4 !== 0) {
+    return false;
+  } else if (num % 4 === 0) {
+    return true;
+  }
+};
