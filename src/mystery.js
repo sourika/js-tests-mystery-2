13b178da2054e1c914282935851184ce13ffdc18
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-11-21 09:27:53 -0800

    Refactor mystery2 function for concise leap year logic

--- src/mystery.js
+++ src/mystery.js
@@ -1,9 +1,2 @@
-export const mystery2 = (num) => {
-  if (num % 400 === 0) {
-    return true;
-  } else if (num % 100 === 0 || num % 4 !== 0) {
-    return false;
-  } else if (num % 4 === 0) {
-    return true;
-  }
-};
+export const mystery2 = (num) => (num % 400 === 0) || (num % 100 !== 0 && num % 4 === 0);
+
